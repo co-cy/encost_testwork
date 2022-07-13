63780c7f15dfa53352a8ed4cbf95084d8dc300df
_co_cy_ <stf2003.kem@gmail.com>
2022-07-13 18:39:37 +0300

    [develop] -> Removed debug trash

--- task3_sql/database/__init__.py
+++ task3_sql/database/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from .tables import BaseTable
 
-engine = create_engine('sqlite:///college.db', echo=True)
+engine = create_engine('sqlite:///college.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 BaseTable.metadata.create_all(engine)
--- task3_sql/parsing.py
+++ task3_sql/parsing.py
@@ -6,11 +6,7 @@ from csv import reader
 def parsing_energy(filename: str):
     with open(filename, "r") as file:
         file.readline()
-        i = 0
         for row in reader(file, delimiter=';'):
             energy = Energy(*row)
             session.add(energy)
-            i += 1
-            if i == 10:
-                break
         session.commit()
