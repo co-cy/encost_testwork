747af22016ef2599a986980f24edc090061c3226
_co_cy_ <stf2003.kem@gmail.com>
2022-07-14 18:34:03 +0300

    [develop] -> Sending

--- task3_sql/database/__init__.py
+++ task3_sql/database/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from .tables import BaseTable
 
-engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/test')
+engine = create_engine('sqlite:///local_test')
 Session = sessionmaker(bind=engine)
 BaseTable.metadata.create_all(engine)
 session = Session()
